efa6719adf1a63f4c3a1233d8a453fce0a79678b
Troy Garraus <troy.garraus@b-2752.mynet>
2019-12-20 17:13:28 +0000

    refactor: create re-usable fetcher function

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,24 @@
 import React, { FC, useEffect, useState } from 'react';
 import './App.scss';
 import Button from './components/Button/Button'
-
-interface AppProps {
-  joke: string
-  isLoaded: boolean
-  error?: string
-}
+import { Fetcher } from './helpers/fetcher'
 
 const App: FC<{}> = () => {
-  const fetchAPI = () => {
-    fetch('https://api.chucknorris.io/jokes/random')
-      .then(res => res.json())
-      .then(res => {
-        if (res.value) {
-          setJoke(res.value)
-          setIsLoaded(true)
-        }
-      }, () => {
+  const apiHost = 'https://api.chucknorris.io/jokes/random'
+  const get = (apiHost: string) => {
+    Fetcher.get(apiHost)
+    .then((res: any) => {
+      setJoke(res.value)
+      setIsLoaded(true)
+    })
+    .catch(err => {
         //TODO: handle error
         setIsLoaded(true)
-      })
+    })
   }
 
   useEffect(() => {
-    fetchAPI()
+    get(apiHost)
   }, [])
 
   const [joke, setJoke] = useState('')
@@ -35,10 +29,11 @@ const App: FC<{}> = () => {
       <section>....is loading</section>
     )
   }
+
   return (
       <>
         <section>{joke}</section>
-        <Button onClick={fetchAPI} text="tell me a joke" />
+        <Button onClick={() => get(apiHost)} text="random joke" />
       </>
   )
 }
---
+++ src/helpers/fetcher.ts
@@ -0,0 +1,21 @@
+const checkStatus = (response: any) => {
+    if (response.ok) {
+        return response
+    } else {
+        const error = new Error(response.statusText)
+        throw error
+    }
+}
+
+const parseJSON = (res: any) => res.json()
+
+const Fetcher = {
+    get: (path: string, params?: {}) =>
+        fetch(path, params)
+        .then(checkStatus)
+        .then(parseJSON)
+
+    //TODO: add for post....
+}
+
+export { Fetcher }
