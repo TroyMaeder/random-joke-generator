ddaaa65ef74f0b3c86c2dbebe249b5220b9326b7
Troy Garraus <troy.garraus@b-2752.mynet>
2019-12-20 19:42:29 +0000

    refactor: move logic to fetch and render joke into own component

---
+++ src/components/JokeGenerator/JokeGenerator.tsx
@@ -0,0 +1,34 @@
+
+import React, { FC, useEffect, useState } from 'react';
+import './App.scss';
+import Button from '../Button/Button'
+import Fetcher from '../../helpers/fetcher'
+
+interface JokeGeneratorProps {
+  baseUrl: string
+}
+
+const JokeGenerator: FC<JokeGeneratorProps> = ({ baseUrl }) => {
+  const get = (apiHost: string) => {
+    Fetcher.get(apiHost)
+    .then((res: any) => {
+      setJoke(res.value)
+    })
+    .catch(err => {
+        //TODO: handle error
+    })
+  }
+
+  useEffect(() => get(baseUrl))
+
+  const [joke, setJoke] = useState('')
+
+  return (
+      <>
+        <section>{joke}</section>
+        <Button onClick={() => get(baseUrl)} text="random joke" />
+      </>
+  )
+}
+
+export default JokeGenerator
